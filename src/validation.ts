c638d6737c6b6db97b868f2e07c19403499a4cb2
isanctuarry <khrnsa143@gmail.com>
2026-03-03 21:06:04 +0700

    Praktikum 1

--- src/validation.ts
+++ src/validation.ts
@@ -35,6 +35,107 @@ app.get(
   }
 )
 
+/*
+app.get(
+  "/products/:id",
+  ({ params, query }) => {
+    return {
+      productId: params.id,
+      sortBy: query.sort
+    }
+  },
+  {
+    params: t.Object({
+      id: t.Number()
+    }),
+    query: t.Object({
+      sort: t.Union([
+        t.Literal("asc"),
+        t.Literal("desc")
+      ])
+    })
+  }
+)
+
+app.get(
+    "/stats",
+    () => {
+      return {
+        total: 100,
+        active: 80
+      }
+    },
+    {
+      response: t.Object({
+        total: t.Number(),
+        active: t.Number()
+      })
+    }
+  )
+
+  .get(
+    "/admin",
+    () => {
+      return {
+        stats: 99
+      }
+    },
+    {
+      beforeHandle({ headers, set }) {
+        if (headers.authorization !== "Bearer 123") {
+          set.status = 401
+          return {
+            success: false,
+            message: "Unauthorized"
+          }
+        }
+      }
+    }
+  )
+
+   .get("/product", () => {
+    return {
+      id: 1,
+      name: "Laptop"
+    }
+  })
+
+  .onAfterHandle(({ response }) => {
+    return {
+      success: true,
+      Message: "data tersedia",
+      data: response
+    }
+  })
+
+.post(
+    "/login",
+    ({ body }) => {
+      return {
+        message: "Login success",
+        user: body.email
+      }
+    },
+    {
+      body: t.Object({
+        email: t.String({ format: "email" }),
+        password: t.String({ minLength: 8 })
+      })
+    }
+  )
+
+  .onError(({ code, set }) => {
+
+    if (code === "VALIDATION") {
+      set.status = 400
+      return {
+        success: false,
+        error: "Validation Error"
+      }
+    }
+  })
+    */
+
   .listen(3000);
 
 
